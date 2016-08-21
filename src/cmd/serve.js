f87c3cf3569ff33aba804580709c0364e84bab97
Charlie Duong <charlieduong94@gmail.com>
2016-08-21 18:04:13 -0400

    Removed check for index.html when running serve command

--- src/cmd/serve.js
+++ src/cmd/serve.js
@@ -27,49 +27,42 @@ module.exports = {
     exec: function() {
         // get options
         var options = _getOptions();
-
-        fs.access('./index.html', READ_PERMISSION, function(err) {
-            if (err) {
-                logger.error('Template not found');
-            } else {
-                build(null, function() {
-                    var server = http.createServer(function(req, res) {
-                        var filePath = process.cwd();
-                        if (req.url === '/') {
-                            filePath += '/index.html';
-                        } else {
-                            filePath += req.url;
-                        }
-                        fs.access(filePath, READ_PERMISSION, function(err) {
-                            if (err) {
-                                res.statusCode = 404;
-                                res.end();
-                            } else {
-                                // prevent caching
-                                res.writeHead(200, {
-                                    'Cache-Control': 'no-cache, no-store, must-revalidate',
-                                    'Pragma': 'no-cache',
-                                    'Expires': '-1'
-                                });
-                                var readStream = fs.createReadStream(filePath);
-                                readStream.pipe(res);
-                            }
+        build(null, function() {
+            var server = http.createServer(function(req, res) {
+                var filePath = process.cwd();
+                if (req.url === '/') {
+                    filePath += '/index.html';
+                } else {
+                    filePath += req.url;
+                }
+                fs.access(filePath, READ_PERMISSION, function(err) {
+                    if (err) {
+                        res.statusCode = 404;
+                        res.end();
+                    } else {
+                        // prevent caching
+                        res.writeHead(200, {
+                            'Cache-Control': 'no-cache, no-store, must-revalidate',
+                            'Pragma': 'no-cache',
+                            'Expires': '-1'
                         });
-                    });
-
-                    var port = options.port;
-                    server.listen(port, function() {
-                        logger.info('Server is now listening on port ' + port);
-                        logger.info('Opening presentation...');
-                        open('http://localhost:' + port);
-                    });
-
-                    fs.watch(process.cwd() + '/index.marko', function(eventType, change){
-                        logger.info('Change detected. Triggering rebuild...');
-                        build();
-                    });
+                        var readStream = fs.createReadStream(filePath);
+                        readStream.pipe(res);
+                    }
                 });
-            }
+            });
+
+            var port = options.port;
+            server.listen(port, function() {
+                logger.info('Server is now listening on port ' + port);
+                logger.info('Opening presentation...');
+                open('http://localhost:' + port);
+            });
+
+            fs.watch(process.cwd() + '/index.marko', function(eventType, change){
+                logger.info('Change detected. Triggering rebuild...');
+                build();
+            });
         });
     }
 };
