255905189fa32856812628e755d3a21020a4ce65
Charlie Duong <charlieduong94@gmail.com>
2017-02-18 00:07:24 -0500

    Add .travis.yml

--- test/run-tests.js
+++ test/run-tests.js
@@ -4,15 +4,15 @@ require('babel-polyfill')
 const Mocha = require('mocha')
 const glob = require('glob')
 
-let mocha = new Mocha({
+const mocha = new Mocha({
   ui: 'bdd',
   reporter: 'list'
 })
 
-let unitTests = glob.sync(`${__dirname}/unit/**/*.js`)
-let integrationTests = glob.sync(`${__dirname}/integration/**/*.js`)
+const unitTests = glob.sync(`${__dirname}/unit/**/*.js`)
+const integrationTests = glob.sync(`${__dirname}/integration/**/*.js`)
 
-let tests = unitTests.concat(integrationTests)
+const tests = unitTests.concat(integrationTests)
 
 tests.forEach((testFile) => {
   mocha.addFile(testFile)
